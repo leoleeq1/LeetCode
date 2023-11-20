414952cf993b872306651e60e83f87511445c580
Kyuhan (Leo) Lee <100849619+leoleeq1@users.noreply.github.com>
2023-11-21 03:03:15 +0900

    Time: 25 ms (17.42%), Space: 22.9 MB (5.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,59 +1,57 @@
 class Solution {
 public:
-    bool isValidBox(vector<vector<char>>& board, int r, int c)
-    {
-        unordered_set<char> s;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int y=0;y<board.size();++y)
+        {
+            for (int x=0;x<board[y].size();++x)
+            {
+                if (ValidateRow(board, y) && ValidateCol(board, x) && ValidateSub(board, y, x))
+                    continue;
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool ValidateRow(vector<vector<char>>& board, int row) {
+        vector<int> nums(9, 0);
+        for (int i=0;i<9;++i)
+        {
+            auto c = board[row][i];
+            if (c == '.')
+                continue;
+            if (++nums[c - '0' - 1] > 1)
+                return false;
+        }
+        return true;
+    }
+    
+    bool ValidateCol(vector<vector<char>>& board, int col) {
+        vector<int> nums(9, 0);
+        for (int i=0;i<9;++i)
+        {
+            auto c = board[i][col];
+            if (c == '.')
+                continue;
+            if (++nums[c - '0' - 1] > 1)
+                return false;
+        }
+        return true;
+    }
+    
+    bool ValidateSub(vector<vector<char>>& board, int row, int col) {
+        vector<int> nums(9, 0);
+        row = (row / 3) * 3;
+        col = (col / 3) * 3;
         for (int y=0;y<3;++y)
         {
             for (int x=0;x<3;++x)
             {
-                char n = board[r+y][c+x];
-                if (n == '.') continue;
-                if (s.count(n) > 0)
-                    return false;
-                s.insert(n);     
-            }
-        }
-        return true;
-    }
-    bool isValidCol(vector<vector<char>>& board, int c)
-    {
-        unordered_set<char> s;
-        for (int i=0;i<9;++i)
-        {
-            char n = board[i][c];
-            if (n == '.') continue;
-            if (s.count(n) > 0)
-                return false;
-            s.insert(n);
-        }
-        return true;
-    }
-    bool isValidRow(vector<vector<char>>& board, int r)
-    {
-        unordered_set<char> s;
-        for (int i=0;i<9;++i)
-        {
-            char n = board[r][i];
-            if (n == '.') continue;
-            if (s.count(n) > 0)
-                return false;
-            s.insert(n);
-        }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        for (int i=0;i<9;++i)
-        {
-            if (!isValidRow(board, i) || !isValidCol(board, i))
-                return false;
-        }
-        
-        for (int r=0;r<9;r+=3)
-        {
-            for (int c=0;c<9;c+=3)
-            {
-                if (!isValidBox(board, r, c))
+                auto c = board[row+y][col+x];
+                if (c == '.')
+                    continue;
+                if (++nums[c - '0' - 1] > 1)
                     return false;
             }
         }
