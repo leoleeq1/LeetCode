b0bfaa01a91c0d9c1c5bd90fdb6447f8053d6b17
Kyuhan (Leo) Lee <100849619+leoleeq1@users.noreply.github.com>
2023-11-20 23:50:01 +0900

    Time: 16 ms (10.17%), Space: 7.6 MB (78.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,16 +3,12 @@ public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length())
             return false;
-        unordered_map<char, int> m[2];
+
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
         for (int i=0;i<s.length();++i)
         {
-            ++m[0][s[i]];
-            ++m[1][t[i]];
-        }
-        
-        for (auto& p : m[0])
-        {
-            if (m[1].find(p.first) == m[1].end() || m[1][p.first] != p.second)
+            if (s[i] != t[i])
                 return false;
         }
         return true;
