b20feb2ce11cf8ebb51e114f7a56240bf4a939fd
Kyuhan (Leo) Lee <100849619+leoleeq1@users.noreply.github.com>
2023-11-25 17:35:47 +0900

    Time: 57 ms (80.81%), Space: 22.9 MB (35.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int amounts[10001];
+    vector<int> memo = vector<int>(10001, 0);
+    int coinChange(vector<int>& coins, int amount) {
+        for (auto c : coins)
+        {
+            if (c > 10000) continue;
+            memo[c] = 1;
+        }
+        auto result = dp(coins, amount);
+        return result <= 10000 ? result : -1;
+    }
+    
     int dp(vector<int>& coins, int amount)
     {
-        if (amount < 0)
-            return 10001;
-        if (amounts[amount] >= 0)
-            return amounts[amount];
-        int m = 10001;
-        for (int n=0;n<coins.size();++n)
-        {
-            m = min(m,dp(coins,amount - coins[n]));
-        }
-        return amounts[amount] = 1 + m;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0)
-            return 0;
-        memset(amounts, -1, sizeof(amounts));
-        amounts[0] = 0;
+        if (amount < 0) return INT_MAX;
+        if (amount == 0) return 0;
+        if (memo[amount]) return memo[amount];
         
-        int result = dp(coins, amount);
-        return result > 10000 ? -1 : result;
+        auto minCoin = INT_MAX;
+        for (auto c : coins)
+        {
+            if (c > amount) continue;
+            auto m = dp(coins, amount - c);
+            minCoin = min(minCoin, m);
+        }
+        
+        return memo[amount] = minCoin > 10000 ? INT_MAX : minCoin + 1;
     }
 };
