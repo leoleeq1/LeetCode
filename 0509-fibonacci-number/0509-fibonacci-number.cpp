f3b9ddc841f87aaadae7eb06ffefb1ece12b0400
Kyuhan (Leo) Lee <100849619+leoleeq1@users.noreply.github.com>
2023-11-25 16:55:16 +0900

    Time: 2 ms (76.72%), Space: 6.5 MB (24.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    int arr[31] = {0,1,};
+    vector<int> v = vector<int>(31, -1);
     int fib(int n) {
-        if (n == 0)
-            return arr[0];
-        else if (n == 1)
-            return arr[1];
-        
-        if (arr[n] == 0)
-            arr[n] = fib(n - 1) + fib(n - 2);
-        return arr[n];
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        if (v[n] != -1)
+            return v[n];
+        return v[n] = fib(n-1) + fib(n-2);
     }
 };
